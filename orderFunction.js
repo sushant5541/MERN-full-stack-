5d0b6b2bf65db3ee9b26463e953a36b197eb5f1d
SushantPatil <sushantdpatil03@gmail.com>
2025-03-12 09:55:00 +0530

    added orderFunction file

---
+++ orderFunction.js
@@ -0,0 +1,47 @@
+// function greeting(name) {
+//     console.log("hello");
+    
+// }
+
+// setInterval(greeting, 5000, "Ram");
+
+let country = ['IND', 'US', 'PAK', 'AUS', 'NZ', 'SL']
+
+// function callback(el) {
+//     console.log(el.toLowerCase());  
+// }
+// country.forEach(callback);
+
+
+// let callback = (el) => console.log(el.toLowerCase());
+// country.forEach(callback);
+
+// country.forEach((el, i) => console.log(el.toLowerCase(), i));
+
+
+let nums = [2,4,5,6,7,8,9,10,11,12,13,14,15]
+
+// nums.forEach(el => console.log(el * el));
+// let out = nums.map(el => el * 5);
+
+// console.log(nums);
+// console.log(out);
+
+
+
+// let out = nums.filter(el => el % 2 === 0);
+// console.log(out);
+
+// let out = nums.reduce((acc, el) => acc + el);
+// console.log(out);
+
+// let out = country.reduceRight((acc, el) => acc + el);
+// console.log(out);
+
+// let people_age = [17,56,38,55];
+// let voters = people_age.every(el => el>=18);
+
+// console.log(voters);
+
+
+
